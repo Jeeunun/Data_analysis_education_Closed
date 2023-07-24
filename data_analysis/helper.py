a15535b708f4b262e4fb825554eb2264608ac61b
Jeeunun <hanjieun7290@gmail.com>
2023-07-24 12:07:31 +0900

    스피어만 상관분석

--- data_analysis/helper.py
+++ data_analysis/helper.py
@@ -187,3 +187,27 @@ def pearson_r(df):
     rdf.set_index('fields', inplace=True)
 
     return rdf
+
+# 스피어만 상관분석
+from scipy.stats import spearmanr
+def spearman_r(df):
+    names = df.columns
+    n = len(names)
+    pv = 0.05
+
+    data = []
+
+    for i in range(0, n):
+        # 기본적으로 i 다음 위치를 의미하지만 i가 마지막 인덱스일 경우 0으로 설정
+        j = i + 1 if i < n - 1 else 0
+
+        fields = names[i] + ' vs ' + names[j]
+        s, p = spearmanr(df[names[i]], df[names[j]])
+        result = p < pv
+
+        data.append({'fields': fields, 'statistic': s, 'pvalue': p, 'result': result})
+
+    rdf = DataFrame(data)
+    rdf.set_index('fields', inplace=True)
+    
+    return rdf
