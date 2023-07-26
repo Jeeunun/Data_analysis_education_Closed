6628bbdcb749ee30b1b31f791c9ae1ab2954573d
Jeeunun <hanjieun7290@gmail.com>
2023-07-26 11:49:41 +0900

    주성분분석 pca

--- data_analysis/helper.py
+++ data_analysis/helper.py
@@ -337,3 +337,106 @@ def ext_ols(data, y, x):
 
     # 리턴
     return (model, fit, summary, table, result, goodness, varstr)
+
+# 주성분분석 결과 토대로 회귀분석 수행하는 함수
+class OlsResult:
+    def __init__(self):
+        self._model = None
+        self._fit = None
+        self._summary = None
+        self._table = None
+        self._result = None
+        self._goodness = None
+        self._varstr = None
+
+    @property
+    def model(self):
+        """
+        분석모델
+        """
+        return self._model
+
+    @model.setter
+    def model(self, value):
+        self._model = value
+
+    @property
+    def fit(self):
+        """
+        분석결과 객체
+        """
+        return self._fit
+
+    @fit.setter
+    def fit(self, value):
+        self._fit = value
+
+    @property
+    def summary(self):
+        """
+        분석결과 요약 보고
+        """
+        return self._summary
+
+    @summary.setter
+    def summary(self, value):
+        self._summary = value
+
+    @property
+    def table(self):
+        """
+        결과표
+        """
+        return self._table
+
+    @table.setter
+    def table(self, value):
+        self._table = value
+
+    @property
+    def result(self):
+        """
+        결과표 부가 설명
+        """
+        return self._result
+
+    @result.setter
+    def result(self, value):
+        self._result = value
+
+    @property
+    def goodness(self):
+        """
+        모형 적합도 보고
+        """
+        return self._goodness
+
+    @goodness.setter
+    def goodness(self, value):
+        self._goodness = value
+
+    @property
+    def varstr(self):
+        """
+        독립변수 보고
+        """
+        return self._varstr
+
+    @varstr.setter
+    def varstr(self, value):
+        self._varstr = value
+
+
+def my_ols(data, y, x):
+    model, fit, summary, table, result, goodness, varstr = ext_ols(data, y, x)
+
+    ols_result = OlsResult()
+    ols_result.model = model
+    ols_result.fit = fit
+    ols_result.summary = summary
+    ols_result.table = table
+    ols_result.result = result
+    ols_result.goodness = goodness
+    ols_result.varstr = varstr
+
+    return ols_result
