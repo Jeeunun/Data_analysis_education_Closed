11c94c660b9a08596bba7fbfcf59b1f7b887cd15
Jeeunun <hanjieun7290@gmail.com>
2023-08-10 10:45:35 +0900

    helper

--- data_analysis/helper.py
+++ data_analysis/helper.py
@@ -491,8 +491,123 @@ class OlsResult:
         self._varstr = value
 
 
-def my_ols(data, y, x):
-    model, fit, summary, table, result, goodness, varstr = ext_ols(data, y, x)
+def myOls(data, y, x):
+    """
+    회귀분석을 수행한다.
+
+    Parameters
+    -------
+    - data : 데이터 프레임
+    - y: 종속변수 이름
+    - x: 독립변수의 이름들(리스트)
+    """
+
+    # 독립변수의 이름이 리스트가 아니라면 리스트로 변환
+    if type(x) != list:
+        x = [x]
+
+    # 종속변수~독립변수1+독립변수2+독립변수3+... 형태의 식을 생성
+    expr = "%s~%s" % (y, "+".join(x))
+
+    # 회귀모델 생성
+    model = ols(expr, data=data)
+    # 분석 수행
+    fit = model.fit()
+
+    # 파이썬 분석결과를 변수에 저장한다.
+    summary = fit.summary()
+
+    # 첫 번째, 세 번째 표의 내용을 딕셔너리로 분해
+    my = {}
+
+    for k in range(0, 3, 2):
+        items = summary.tables[k].data
+        # print(items)
+
+        for item in items:
+            # print(item)
+            n = len(item)
+
+            for i in range(0, n, 2):
+                key = item[i].strip()[:-1]
+                value = item[i+1].strip()
+
+                if key and value:
+                    my[key] = value
+
+    # 두 번째 표의 내용을 딕셔너리로 분해하여 my에 추가
+    my['variables'] = []
+    name_list = list(data.columns)
+    #print(name_list)
+
+    for i, v in enumerate(summary.tables[1].data):
+        if i == 0:
+            continue
+
+        # 변수의 이름
+        name = v[0].strip()
+
+        vif = 0
+
+        # Intercept는 제외
+        if name in name_list:
+            # 변수의 이름 목록에서 현재 변수가 몇 번째 항목인지 찾기 
+            j = name_list.index(name)
+            vif = variance_inflation_factor(data, j)
+
+        my['variables'].append({
+            "name": name,
+            "coef": v[1].strip(),
+            "std err": v[2].strip(),
+            "t": v[3].strip(),
+            "P-value": v[4].strip(),
+            "Beta": 0,
+            "VIF": vif,
+        })
+
+    # 결과표를 데이터프레임으로 구성
+    mylist = []
+    yname_list = []
+    xname_list = []
+
+    for i in my['variables']:
+        if i['name'] == 'Intercept':
+            continue
+
+        yname_list.append(y)
+        xname_list.append(i['name'])
+
+        item = {
+            "B": i['coef'],
+            "표준오차": i['std err'],
+            "β": i['Beta'],
+            "t": "%s*" % i['t'],
+            "유의확률": i['P-value'],
+            "VIF": i["VIF"]
+        }
+
+        mylist.append(item)
+
+    table = DataFrame(mylist,
+                   index=MultiIndex.from_arrays([yname_list, xname_list], names=['종속변수', '독립변수']))
+    
+    # 분석결과
+    result = "𝑅(%s), 𝑅^2(%s), 𝐹(%s), 유의확률(%s), Durbin-Watson(%s)" % (my['R-squared'], my['Adj. R-squared'], my['F-statistic'], my['Prob (F-statistic)'], my['Durbin-Watson'])
+
+    # 모형 적합도 보고
+    goodness = "%s에 대하여 %s로 예측하는 회귀분석을 실시한 결과, 이 회귀모형은 통계적으로 %s(F(%s,%s) = %s, p < 0.05)." % (y, ",".join(x), "유의하다" if float(my['Prob (F-statistic)']) < 0.05 else "유의하지 않다", my['Df Model'], my['Df Residuals'], my['F-statistic'])
+
+    # 독립변수 보고
+    varstr = []
+
+    for i, v in enumerate(my['variables']):
+        if i == 0:
+            continue
+        
+        s = "%s의 회귀계수는 %s(p%s0.05)로, %s에 대하여 %s."
+        k = s % (v['name'], v['coef'], "<" if float(v['P-value']) < 0.05 else '>', y, '유의미한 예측변인인 것으로 나타났다' if float(v['P-value']) < 0.05 else '유의하지 않은 예측변인인 것으로 나타났다')
+
+        varstr.append(k)
 
     ols_result = OlsResult()
     ols_result.model = model
@@ -504,6 +619,7 @@ def my_ols(data, y, x):
     ols_result.varstr = varstr
 
     return ols_result
+
 
 
 # 독립변수, 종속변수 표준화 적용 함수
